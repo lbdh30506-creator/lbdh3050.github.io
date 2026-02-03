9031194508a5b2c37dad9df4e1aa7c4724da2766
lbdh30506-creator <lbdh30506@gmail.com>
2026-02-03 22:31:54 +0300

    Create hshshsh.hh
    تريمكس

---
+++ hshshsh.hh
@@ -0,0 +1,62 @@
+# Termux Banner
+<pre>Cool Evil Eye Banner Script for Termux ;)</pre>
+
+
+<p float="center">
+  <img src="https://i.ibb.co/dJGNrTcm/image.png" width="400" height="300" />
+  <img src="https://raw.githubusercontent.com/Bhai4You/bhai4you/master/msg5780888591-38935.jpg" width="400" height="300" /> 
+</p>
+
+## Easy Installation :
+
+paste below command in termux for install termux banner.
+<pre>
+  pkg install python -y && pip install termux-banner && banner
+</pre>
+
+## How to Remove ? :
+
+just type remove command in termux for restore old termux theme.
+<pre>
+  remove
+</pre>
+
+## Issue / Feedback :
+
+<pre>
+  you can contact us through telegram username : @realbhai4you
+  i would like to help you during installation of termux banner ;)
+</pre>
+
+
+## Tutorial :
+<p>
+  <a href="https://bhai4you.blogspot.com/2021/12/evileye2.html">Easy New Method</a>
+  </p>
+
+## Termux Toolbox App (AD)
+<center><a href="https://i.ibb.co/dJGNrTcm/image.png" alt="Termux Toolbox"  ></a></center>
+<pre> 
+  Features :
+  - Cool UI
+  - Category Wise Tools
+  - Quick Linux Commands
+  - Basic to Advance Guide
+  - Powerful Termux Quiz
+  - AI Script Maker - Mr.Robot AI
+  - Programming Tutorials with Termux (included all language)
+  - 200+ Working Termux Tools
+  - Install any tool in termux with single commands.
+  - and much more..
+</pre>
+
+## Contribution :
+<pre>
+🎨 Calling All Creative Minds! Contribute to Termux-Banner Project
+  
+Are you a design enthusiast with an eye for creativity? 
+Join the Termux-Banner project and give it a fresh new look! 
+We're on a mission to elevate the visual experience of Termux users, and we need your expertise.
+
+Let's give Termux users a reason to smile every time they open the terminal. 
+</pre>
